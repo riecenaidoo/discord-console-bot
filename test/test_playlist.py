780410c072597adfc823f568500d6dd7a23ae135
riecenaidoo <riecenaidoo@outlook.com>
2023-11-19 16:28:28 +0200

    using abs path to fix import

--- test/test_playlist.py
+++ test/test_playlist.py
@@ -1,6 +1,10 @@
 import unittest
 
-from src.bot.playlist import Playlist
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import src.bot.playlist as Playlist
 
 
 class TestMusicQueue(unittest.TestCase):
